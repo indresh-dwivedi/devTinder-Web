1bc892ac8ddbf3dfac931f049ad3867c07609a0d
indresh-007 <indresh209721@gmail.com>
2026-07-30 13:38:46 +0530

    implement routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-function App() {
-  
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./Body";
+import Login from "./Login";
+import  Profile from "./Profile";
 
+function App() {
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-          Hello world!
-      </h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
