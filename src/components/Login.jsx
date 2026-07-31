169123531934ff3d8b1e8a536327b3975febc533
indresh-007 <indresh209721@gmail.com>
2026-08-01 00:29:27 +0530

    implement logout feature

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,6 +8,7 @@ import { BASE_URL } from "../utils/constants";
 const Login = () => {
     const [emailId, setEmailId] = useState("");
     const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
     const dispatch = useDispatch();
     const navigate = useNavigate();
     
@@ -22,7 +23,7 @@ const Login = () => {
             dispatch(addUser(res.data));
             return navigate("/");
         } catch (err) {
-            console.error(err);
+            setError(err?.response?.data || "Something went wrong");
         }
     };
 
@@ -55,6 +56,7 @@ const Login = () => {
                 />
             </label>
           </div>
+          <p className="text-red-500">{error}</p>
           <div className="card-actions justify-center m-2">
             <button className="btn btn-primary" onClick={handleLogin}>
                 Login
